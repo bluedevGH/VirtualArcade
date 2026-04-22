175f6ba8a8f6de9c43bfc30fbfb491b42ad1815e
renshei <100230983@student.burnley.ac.uk>
2026-04-22 19:23:46 +0100

    New theme options templated

--- public/src/themes.js
+++ public/src/themes.js
@@ -1,1 +1,16 @@
-// placeholder for theme switching
+console.log("hi im sprint and going")
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('body').addEventListener('click', (e) => {
+        switch (e.target.id) {
+            case 'light':
+                document.body.className = 'theme-default';
+                console.log('light');
+                break;
+            case 'dark':
+                document.body.className = 'theme-dark';
+                console.log('dark');
+                break;
+        }
+    });
+});
